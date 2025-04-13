32577b9a6424f368dd8c9b99784a09fe4b7f03d4
Anjali <veranjali2503@gmail.com>
2025-04-13 21:54:06 +0530

    Initial commit
    Generated by react-native@0.79.0

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+  Text,
+  View,
+  SafeAreaView
+} from 'react-native'
+
+const App = () =>{
+  return(
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  )
+}
